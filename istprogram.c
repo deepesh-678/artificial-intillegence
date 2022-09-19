917b8c77f86bfce88f533883d687a51706aa9661
deepesh-678 <113338558+deepesh-678@users.noreply.github.com>
2022-09-19 09:50:45 +0530

    Add files via upload

---
+++ istprogram.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    int a = 9, b = 4, c;
+
+    c = a+b;
+    printf("a+b = %d\n", c);
+    c = a-b;
+    printf("a-b = %d\n", c);
+    c = a*b;
+    printf("a*b = %d\n",c);
+    c = a/b;
+    printf("a/b = %d\n", c);
+    c = a%b;
+    printf("a%b = %d\n", c);
+
+    return 0;
+}
+
+compile and execute the c program to find the factorial of a positive integer 
